025863353cb717175b7557cfaf55941ec79f4c4d
맥북 <mac@maegbug-ui-MacBookAir.local>
2022-09-30 19:36:17 +0900

    first commit

---
+++ app/src/main/java/com/example/myjavaworld/myjavaapp.kt
@@ -0,0 +1,21 @@
+package com.example.myjavaworld
+
+//Array
+//List 1. List 2. MutableList
+fun main(){
+    array()
+    println("DDD")
+}
+fun array(){
+    val array = arrayOf(1,2,3)
+    val list = listOf(1,2,3)
+
+    val array2 = arrayOf(1,"d",3,4f)
+    val list2 = listOf(1,"d",11L)
+
+    array[0] = 2
+
+    println("DDDD")
+
+
+}
